650a9aa9b0271641d047a4143be05573144065b3
emrgem <emrgem@bergen.org>
2025-04-02 14:57:29 -0400

    selector practice 1 done

--- Unit5-DOM/Lesson2-Element-Selectors/practice - 1/script.js
+++ Unit5-DOM/Lesson2-Element-Selectors/practice - 1/script.js
@@ -1,7 +1,11 @@
 // Select all <div> elements with class "box" and:
 // change background color to "lightblue"
-
+document.querySelectorAll(".box").forEach(box =>{
+    box.style.backgroundColor = 'lightblue'
+    box.style.color = 'white'
+})
 
 
 // Select <ul> element with ID "list" and 
 //change: text color of all <li> children to "green"
+document.querySelector("#list").querySelectorAll("li").forEach(item => item.style.color = "green")
