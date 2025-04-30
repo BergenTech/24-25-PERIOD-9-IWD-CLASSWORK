61f5174843c4b35a00785c19f9ca46db394c8ce8
emrgem <emrgem@bergen.org>
2025-04-30 14:57:13 -0400

    input events

---
+++ Unit5-DOM/Lesson5-Events/Part5-Input Events/script.js
@@ -0,0 +1,42 @@
+// =======================================================
+// Section 1: Input Event
+// =======================================================
+const inputDemo = document.querySelector('#inputDemo')
+const inputOutput = document.querySelector('#inputOutput')
+
+inputDemo.addEventListener('input', function(e){
+    inputOutput.textContent = `Current Value: ${e.target.value}`
+
+})
+// =======================================================
+// Section 2: Change Event
+// =======================================================
+// const changeDemo = document.querySelector("#changeDemo")
+// const changeOutput = document.querySelector("#changeOutput")
+// changeDemo.addEventListener('change', function(e){
+//     changeOutput.textContent = `Value committed: ${e.target.value}`
+// })
+
+document.querySelector("#changeDemo").addEventListener('change', function(e){
+    document.querySelector("#changeOutput").textContent = `Value committed: ${e.target.value}`
+})
+
+document.querySelector("#selectDemo").addEventListener("change", function(e){
+    document.querySelector("#selectOutput").textContent = `Selected option: ${e.target.value}`
+})
+// =======================================================
+// Section 3: Focus and Blur Events
+// =======================================================
+document.getElementById('focusDemo').addEventListener("focus", function(e){
+    document.querySelector("#focusOutput").textContent = `Input Field is now focused!`
+    document.getElementById('focusDemo').style.color = 'white'
+    document.getElementById('focusDemo').style.backgroundColor = 'black'
+})
+document.getElementById('focusDemo').addEventListener("blur", function(e){
+    document.querySelector("#focusOutput").textContent = `Input Field is now focused!`
+    document.getElementById('focusDemo').style.color = 'black'
+    document.getElementById('focusDemo').style.backgroundColor = 'yellow'
+})
+
+
+// Basic focus and blur events
