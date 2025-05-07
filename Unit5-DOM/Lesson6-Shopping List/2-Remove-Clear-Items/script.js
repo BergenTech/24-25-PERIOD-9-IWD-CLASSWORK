3759b59564501fb4afaa65382d17d27f4e1d8cc7
emrgem <emrgem@bergen.org>
2025-05-07 14:41:08 -0400

    remove item

---
+++ Unit5-DOM/Lesson6-Shopping List/2-Remove-Clear-Items/script.js
@@ -0,0 +1,57 @@
+// DOM ELEMENTS
+const itemForm = document.getElementById("item-form")
+const itemInput = document.querySelector("#item-input")
+const itemList = document.querySelector("#item-list")
+
+//Function to add a new item
+function addItem(e){
+  e.preventDefault() // Stop the form from refreshing
+  // console.log("The form works fine!")
+  //Step 1 - Get the input value and trim it
+  const newItem = itemInput.value.trim()
+  // console.log(newItem)
+  
+  //Step 2 - Warn the user if input text is empty
+  if(newItem === ""){
+    alert("Please enter an item!")
+    return  //Stop the process
+  }
+
+  //Step 3 - Create a new list item
+  //Alternative way
+  const li = document.createElement('li')
+  li.innerHTML = `
+         ${newItem}
+          <button class="remove-item btn-link text-red">
+            <i class="fa-solid fa-xmark"></i>
+          </button>
+  `
+  itemList.appendChild(li)
+  itemInput.value = ""
+}
+
+//Function to remove an item
+function removeItem(e){
+  //check if the click element is the X button or it's icon
+  if(e.target.tagName ==='I' && e.target.classList.contains('fa-xmark')){
+    // console.log("Clicked on an X!")
+    //confirm before deleting
+    if(confirm("Are you sure you want to remove this item?")){
+      //Get the parent li and remove it
+      e.target.parentElement.parentElement.remove()
+    }
+  }
+}
+
+//Function to Initialize the application
+function initApp(){
+  //Add item event
+  itemForm.addEventListener('submit', addItem)
+
+  //Remove item event
+  itemList.addEventListener('click', removeItem)
+
+}
+
+//Call the initApp
+initApp()
